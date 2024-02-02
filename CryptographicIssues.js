ac214f0065d41ac289f9f37241b2650a98676abc
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-02-02 12:15:03 +0530

    Update and rename CryptographicIssues.ts to CryptographicIssues.js

---
+++ CryptographicIssues.js
@@ -0,0 +1,34 @@
+'use strict';
+    var crypto = require('crypto');
+
+    /**
+     * generates random string of characters i.e salt
+     * @function
+     * @param {number} length - Length of the random string.
+     */
+    function genRandomString (length){
+        return crypto.randomBytes(Math.ceil(length/2))
+                .toString('hex') /** convert to hexadecimal format */
+                .slice(0,length);   /** return required number of characters */
+    };
+
+    /**
+     * hash password with sha512.
+     * @function
+     * @param {string} password - List of required fields.
+     * @param {string} salt - Data to be validated.
+     */
+    function sha512(password, salt){
+        var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+        hash.update(password);
+        var value = hash.digest('hex');
+        return {
+            salt:salt,
+            passwordHash:value
+        };
+    };
+
+    module.exports = {
+        genRandomString: genRandomString,
+        sha512: sha512
+    };
