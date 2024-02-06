179efc65e52714b29cec9e078c7cc580b50f7d9e
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-02-06 12:06:48 +0530

    Update CryptographicIssues.ts

--- CryptographicIssues.ts
+++ CryptographicIssues.ts
@@ -3,7 +3,7 @@ import * as crypto from 'crypto';
 class YourClass {
   private m_mgf1_hash: string;
   private m_alg_hash: string;
-  private m_private_key: crypto.KeyObject; // Assuming this is a KeyObject
+  private m_private_key: crypto.KeyObject;
   private m_algo_param: crypto.OAEPOptions;
   private m_cipher: crypto.Cipher;
 
@@ -15,7 +15,8 @@ class YourClass {
     this.m_cipher.init(crypto.constants.DECRYPT, this.m_private_key, this.m_algo_param);
     const java_plaintext = Buffer.concat([this.m_cipher.update(ciphertext), this.m_cipher.final()]);
 
-    if (!java_plaintext.equals(plaintext)) {
+    // Use crypto.timingSafeEqual for constant-time comparison
+    if (!crypto.timingSafeEqual(java_plaintext, plaintext)) {
       throw new Error('Verification failure - plaintext does not match after decryption.');
     }
   }
