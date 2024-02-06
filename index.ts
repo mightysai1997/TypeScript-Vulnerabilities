a7ef5b5feca2b3848952b058b2c30c9a9d189968
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-02-06 12:17:37 +0530

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,8 +1,19 @@
-var express = require('express')
-var app = express()
-function improperRestrictionOfFramesNoncompliant() {
-    app.use((req, res) => {
-        // Noncompliant: it has broken `X-Frame-Options` header.
-        res.setHeader("X-Frame-Options", req.query)
-    })
+function cryptographicKeyGeneratorNoncompliant()
+{
+    var crypto = require("crypto")
+    var object = {
+        // Noncompliant: 'modulusLength' is less than 2048 bits.
+        modulusLength: 1024,
+        publicKeyEncoding: {
+            type: 'spki',
+            format: 'pem'
+        },
+        privateKeyEncoding: {
+            type: 'pkcs8',
+            format: 'pem',
+            cipher: 'aes-256-cbc',
+            passphrase: 'top secret'
+        }
+    }
+    var { publicKey, privateKey} = crypto.generateKeyPairSync('rsa',object)
 }
