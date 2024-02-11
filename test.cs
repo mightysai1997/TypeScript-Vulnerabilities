e6ee9648aacfd3b32bb7b052eae224e952d1e701
mightysai1997 <110396758+mightysai1997@users.noreply.github.com>
2024-02-11 13:51:27 +0530

    Update test.cs

--- test.cs
+++ test.cs
@@ -1,71 +1,180 @@
-using System;
-using System.Security.Cryptography;
-					
-public class Program
-{	
-	public void GenerateBadKey() {
-		var rng = new System.Random();
-		byte[] key = new byte[16];
-		rng.NextBytes(key);
-		SymmetricAlgorithm cipher = Aes.Create();
-		// ruleid: use_weak_rng_for_keygeneration
-		cipher.Key = key;
-	}
-	
-	public void GenerateBadKeyGcm() {
-		var rng = new System.Random();
-		byte[] key = new byte[16];
-		rng.NextBytes(key);
-		// ruleid: use_weak_rng_for_keygeneration
-		var cipher = new AesGcm(key);
-	}
-	
-	public void GenerateGoodKey() {
-		var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
-		byte[] key = new byte[16];
-		rng.GetBytes(key);
-		var cipher = Aes.Create();
-		// ok: use_weak_rng_for_keygeneration
-		cipher.Key = key;
-	}
+using System.Diagnostics;
 
-	public void GenerateGoodKeyGcm() {
-		var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
-		byte[] key = new byte[16];
-		rng.GetBytes(key);
-		// ok: use_weak_rng_for_keygeneration
-		var cipher = new AesGcm(key);
-	}
+namespace Injections
+{
+    public class OsCommandInjection
+    {
+        public void RunOsCommand(string command)
+        {
+            // ruleid: os-command-injection
+            var process = Process.Start(command);
+        }
 
-	public void GenerateBadKeyCcm() {
-		var rng = new System.Random();
-		byte[] key = new byte[16];
-		rng.NextBytes(key);
-		// ruleid: use_weak_rng_for_keygeneration
-		var cipher = new AesCcm(key);
-	}
+        public void RunOsCommand(string command)
+        {
+            // ok: os-command-injection
+            var process = Process.Start("constant");
+        }
 
-	public void GenerateGoodKeyCcm() {
-		var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
-		byte[] key = new byte[16];
-		rng.GetBytes(key);
-		// ok: use_weak_rng_for_keygeneration
-		var cipher = new AesCcm(key);
-	}
 
-	public void GenerateBadKeyChaCha20() {
-		var rng = new System.Random();
-		byte[] key = new byte[16];
-		rng.NextBytes(key);
-		// ruleid: use_weak_rng_for_keygeneration
-		var cipher = new ChaCha20Poly1305(key);
-	}
+        public void RunOsCommandWithArgs(string command, string arguments)
+        {
+            // ruleid: os-command-injection
+            var process = Process.Start(command, arguments);
+        }
 
-	public void GenerateGoodKeyChaCha20() {
-		var rng = System.Security.Cryptography.RandomNumberGenerator.Create();
-		byte[] key = new byte[16];
-		rng.GetBytes(key);
-		// ok: use_weak_rng_for_keygeneration
-		var cipher = new ChaCha20Poly1305(key);
-	}
+        public void RunOsCommandWithArgs(string command, string arguments)
+        {
+            // ok: os-command-injection
+            var process = Process.Start("constant", "constant");
+        }
+
+
+        public void RunOsCommandWithProcessParam(string command)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = command;
+            // ruleid: os-command-injection
+            process.Start();
+        }
+
+        public void RunOsCommandWithProcessParam(string command)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = "constant";
+            // ok: os-command-injection
+            process.Start();
+        }
+
+
+        public void RunOsCommandAndArgsWithProcessParam(string command, string arguments)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = command;
+            process.StartInfo.Arguments = arguments;
+            // ruleid: os-command-injection
+            process.Start();
+        }
+
+        public void RunOsCommandAndArgsWithProcessParam(string command, string arguments)
+        {
+            Process process = new Process();
+
+            process.StartInfo.FileName = "constant";
+            process.StartInfo.Arguments = "constant";
+            // ok: os-command-injection
+            process.Start();
+        }
+
+
+        public void RunOsCommandWithStartInfo(string command)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = command
+            };
+
+            // ruleid: os-command-injection
+            var process = Process.Start(processStartInfo);
+        }
+
+        public void RunOsCommandWithStartInfo(string command)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = "constant"
+            };
+
+            // ok: os-command-injection
+            var process = Process.Start(processStartInfo);
+        }
+
+
+        public void RunConstantAppWithArgs(string args)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = "constant",
+                Arguments = args
+            };
+
+            // ruleid: os-command-injection
+            var process = Process.Start(processStartInfo);
+        }
+
+        public void RunConstantAppWithArgs(string args)
+        {
+            ProcessStartInfo processStartInfo = new ProcessStartInfo()
+            {
+                FileName = "constant",
+                Arguments = "constant"
+            };
+
+            // ok: os-command-injection
+            var process = Process.Start(processStartInfo);
+        }
+
+        public void RunOsCommandAndArgsWithProcessParam(string command, string arguments)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = args
+                }
+            };
+
+            // ruleid: os-command-injection
+            process.Start();
+        }
+
+        public void RunOsCommandAndArgsWithProcessParam(string command, string arguments)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "constant",
+                    Arguments = arguments
+                }
+            };
+
+            // ruleid: os-command-injection
+            process.Start();
+        }
+
+        public void RunOsCommandAndArgsWithProcessParam(string command, string arguments)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = command,
+                    Arguments = "constant"
+                }
+            };
+
+            // ruleid: os-command-injection
+            process.Start();
+        }
+
+        public void RunOsCommandAndArgsWithProcessParam(string command, string arguments)
+        {
+            Process process = new Process
+            {
+                StartInfo = new ProcessStartInfo
+                {
+                    FileName = "constant",
+                    Arguments = "constant"
+                }
+            };
+
+            // ok: os-command-injection
+            process.Start();
+        }
+    }
 }
